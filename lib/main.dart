f3dff76dddec31e31aaf739402d091276b6a7e54
Carlos Hidalgo <cahidalgo18@ucatolica.edu.co>
2026-03-13 18:54:21 -0500

    Creacion del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vendedores TM',
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: Text('¡Proyecto listo!'),
+        ),
+      ),
+    );
+  }
+}
